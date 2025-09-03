d8e9e0e8b5de8cb1a96a074c8d45560c09db04af
Quang Duong <quang.duong@deveritec.com>
2025-09-03 11:09:36 +0200

    Add pre-commit hooks and formatter

--- app/src/main.c
+++ app/src/main.c
@@ -5,8 +5,8 @@
  */
 
 #include <stdio.h>
-#include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(main);
@@ -25,35 +25,31 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
-	int ret;
-	bool led_state = true;
+    int ret;
+    bool led_state = true;
 
-	LOG_INF("Starting the application...");
+    LOG_INF("Starting the application...");
 
-	if (!gpio_is_ready_dt(&led))
-	{
-		return 0;
-	}
+    if (!gpio_is_ready_dt(&led)) {
+        return 0;
+    }
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0)
-	{
-		return 0;
-	}
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        return 0;
+    }
 
-	LOG_INF("GPIO configuration done.");
+    LOG_INF("GPIO configuration done.");
 
-	while (1)
-	{
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0)
-		{
-			return 0;
-		}
+    while (1) {
+        ret = gpio_pin_toggle_dt(&led);
+        if (ret < 0) {
+            return 0;
+        }
 
-		led_state = !led_state;
-		LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-		k_msleep(SLEEP_TIME_MS);
-	}
-	return 0;
+        led_state = !led_state;
+        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
+        k_msleep(SLEEP_TIME_MS);
+    }
+    return 0;
 }
