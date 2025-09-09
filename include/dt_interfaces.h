765a3be716898aa339effaf8ffb81c1dc6d110f2
Quang Duong <quang.duong1910@gmail.com>
2025-09-10 00:09:33 +0200

    Add library wrapper and example for gpio

--- include/dt_interfaces.h
+++ include/dt_interfaces.h
@@ -12,10 +12,33 @@
 #include <zephyr/kernel.h>
 
 /* LED Indicator */
-#define INDICATOR_NODE_0 DT_ALIAS(led0)
+#define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
 
-#if !DT_NODE_HAS_STATUS(INDICATOR_NODE_0, okay)
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
     #error "Unsupported board: led0 devicetree node label is not defined"
+#endif
+
+#if !DT_NODE_HAS_STATUS(LED1_NODE, okay)
+    #error "Unsupported board: led1 devicetree node label is not defined"
+#endif
+
+#if !DT_NODE_HAS_STATUS(LED2_NODE, okay)
+    #error "Unsupported board: led2 devicetree node label is not defined"
+#endif
+
+/* Outputs */
+#define CODEC_IRQ_NODE DT_ALIAS(codec_irq)
+
+#if !DT_NODE_HAS_STATUS(CODEC_IRQ_NODE, okay)
+    #error "Unsupported board: codec_irq devicetree node label is not defined"
+#endif
+
+#define DRC_IN2_NODE DT_ALIAS(drc_in_2)
+
+#if !DT_NODE_HAS_STATUS(DRC_IN2_NODE, okay)
+    #error "Unsupported board: drc_in_2 devicetree node label is not defined"
 #endif
 
 /* UART */
@@ -30,6 +53,13 @@
 
 #if !DT_NODE_HAS_STATUS(VOLTAGE_DIVIDER_NODE, okay)
     #error "Unsupported board: voltage_divider devicetree node label is not defined"
+#endif
+
+/* Sensor/Chip */
+#define EXT_RTC_NODE DT_NODELABEL(extrtc0)
+
+#if !DT_NODE_HAS_STATUS(EXT_RTC_NODE, okay)
+    #error "Unsupported board: extrtc0 devicetree node label is not defined"
 #endif
 
 #endif /* FIRMWARE_APPLICATION_SRC_DT_INTERFACES_H_ */
---
+++ lib/gpio/gpio_wrapper.c
@@ -0,0 +1,62 @@
+#include "gpio_wrapper.h"
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(gpiow, CONFIG_GPIO_LOG_LEVEL);
+
+int gpiow_init(struct gpiow* gw, const struct gpio_dt_spec* spec, gpiow_dir_t dir, gpio_flags_t flags)
+{
+    if (!gw || !spec) {
+        return -EINVAL;
+    }
+
+    if (!gpio_is_ready_dt(spec)) {
+        return -ENODEV;
+    }
+
+    gw->spec = spec;
+    gw->dir = dir;
+
+    int ret;
+    if (dir == GPIOW_DIR_OUTPUT) {
+        ret = gpio_pin_configure_dt(spec, GPIO_OUTPUT | flags);
+    }
+    else {
+        ret = gpio_pin_configure_dt(spec, GPIO_INPUT | flags);
+    }
+
+    return ret;
+}
+
+int gpiow_add_callback(struct gpiow* gw, gpiow_callback_t cb, gpio_flags_t edge)
+{
+    if (!gw || gw->dir != GPIOW_DIR_INPUT) {
+        return -EINVAL;
+    }
+
+    int ret = gpio_pin_interrupt_configure_dt(gw->spec, edge);
+    if (ret) {
+        return ret;
+    }
+
+    gpio_init_callback(&gw->cb_data, (gpio_callback_handler_t)cb, BIT(gw->spec->pin));
+    gpio_add_callback(gw->spec->port, &gw->cb_data);
+
+    return 0;
+}
+
+int gpiow_write(struct gpiow* gw, int value)
+{
+    if (!gw || gw->dir != GPIOW_DIR_OUTPUT) {
+        return -EINVAL;
+    }
+    return gpio_pin_set_dt(gw->spec, value);
+}
+
+int gpiow_toggle(struct gpiow* gw)
+{
+    if (!gw || gw->dir != GPIOW_DIR_OUTPUT) {
+        return -EINVAL;
+    }
+    return gpio_pin_toggle_dt(gw->spec);
+}
